54937d00247f16c466beab5a33ac1d4903b8f3bc
Mae Large <mlarge@vmware.com>
2022-02-28 06:34:24 -0700

    nudge

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "supply chain journey starts here";
+		return "custom supply chain journey starts here";
 	}
 
 }
